015d5f7f196e9ff95f0bed37e63b913f037058b4
clement2019 <ayeniayokunle@yahoo.co.uk>
2024-03-18 16:58:50 +0100

    modified readme

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("This is my express app");
+  res.send("This is my express application");
 });
 
 app.get("/me", (req, res) => {
-  res.send("Hi I am Laith");
+  res.send("Hi I am  clement");
 });
 
 app.listen(5000, () => {
