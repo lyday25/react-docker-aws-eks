b84c1e0de97e3d6def570db8920e565d503a7be8
lyday25 <bilqisalayaki@gmail.com>
2024-03-19 12:12:48 +0100

    modified index file

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("This is my express application");
+  res.send("This is my first react application");
 });
 
 app.get("/me", (req, res) => {
-  res.send("Hi I am  clement");
+  res.send("Hi I am  Laide");
 });
 
 app.listen(5004, () => {
